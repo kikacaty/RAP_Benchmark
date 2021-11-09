6c9aff7ef804a22f12f5f10942a7ec022e3e4460
Yulong Cao <yulongc@umich.edu>
2021-11-09 18:57:33 -0500

    update init

--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-pip install torchvision==0.8.2
+pip install torch==1.7.1+cu101 torchvision==0.8.2+cu101 torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
 pip install timm==0.3.2
 pip install mmcv-full==1.2.7
 pip install opencv-python==4.5.1.48
